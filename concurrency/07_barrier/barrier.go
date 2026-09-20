d3e12763f57dc31d9c4b06c83100661597be5d9b
p-zh-tech <kisly02@gmail.com>
2026-09-20 23:32:02 +0300

    Add barrier implementation with wait method
    Implement barrier functionality and handle wait logic.

--- concurrency/07_barrier/barrier.go
+++ concurrency/07_barrier/barrier.go
@@ -1,5 +1,10 @@
 package barrier
 
+import (
+	"sync/atomic"
+	"primitives/internal/futex"
+)
+
 type Barrier struct {
 	need    uint32
 	arrived uint32
@@ -7,9 +12,33 @@ type Barrier struct {
 }
 
 func New(n int) *Barrier {
-	panic("не реализовано")
+	if n <= 0 {
+		panic("barrier size must be positive")
+	}
+
+	return &Barrier{
+		need: uint32(n),
+	}
 }
 
 func (b *Barrier) Wait() {
-	panic("не реализовано")
+
+	myRound := atomic.LoadUint32(&b.round)
+
+	arrived := atomic.AddUint32(&b.arrived, 1)
+
+	if arrived == b.need {
+		atomic.StoreUint32(&b.arrived, 0)
+		atomic.AddUint32(&b.round, 1)
+		futex.WakeAll(&b.round)
+		return
+	}
+
+	for {
+		if atomic.LoadUint32(&b.round) != myRound {
+			return
+		}
+
+		futex.Wait(&b.round, myRound)
+	}
 }
