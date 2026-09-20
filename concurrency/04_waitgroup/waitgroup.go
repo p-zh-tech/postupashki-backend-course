813507d2130baebccf618bde0878425065727dd5
p-zh-tech <kisly02@gmail.com>
2026-09-20 23:30:22 +0300

    Implement WaitGroup methods for concurrency control

--- concurrency/04_waitgroup/waitgroup.go
+++ concurrency/04_waitgroup/waitgroup.go
@@ -1,17 +1,45 @@
 package waitgroup
 
+import (
+	"primitives/internal/futex"
+	"sync/atomic"
+)
+
 type WaitGroup struct {
 	count uint32
 }
 
 func (wg *WaitGroup) Add(delta int) {
-	panic("не реализовано")
+	for {
+		old := atomic.LoadUint32(&wg.count)
+
+		if delta < 0 && uint32(-delta) > old {
+			panic("negative WaitGroup counter")
+		}
+
+		newCount := uint32(int64(old) + int64(delta))
+
+		if atomic.CompareAndSwapUint32(&wg.count, old, newCount) {
+			if newCount == 0 && old != 0 {
+				futex.WakeAll(&wg.count)
+			}
+			return
+		}
+	}
 }
 
 func (wg *WaitGroup) Done() {
-	panic("не реализовано")
+	wg.Add(-1)
 }
 
 func (wg *WaitGroup) Wait() {
-	panic("не реализовано")
+	for {
+		count := atomic.LoadUint32(&wg.count)
+
+		if count == 0 {
+			return
+		}
+
+		futex.Wait(&wg.count, count)
+	}
 }
