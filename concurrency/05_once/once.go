0ebc9c04b31623d032bb9f2b55b767df7c293569
p-zh-tech <kisly02@gmail.com>
2026-09-20 23:31:01 +0300

    Implement Once.Do and Once.Done methods

--- concurrency/05_once/once.go
+++ concurrency/05_once/once.go
@@ -1,13 +1,46 @@
 package once
 
+import (
+	"sync/atomic"
+
+	"primitives/internal/futex"
+)
+
+const (
+	idle = iota
+	running
+	done
+)
+
 type Once struct {
 	state uint32
 }
 
 func (o *Once) Do(f func()) {
-	panic("не реализовано")
+	if atomic.LoadUint32(&o.state) == done {
+		return
+	}
+
+	if atomic.CompareAndSwapUint32(&o.state, idle, running) {
+		defer func() {
+			atomic.StoreUint32(&o.state, done)
+			futex.WakeAll(&o.state)
+		}()
+
+		f()
+		return
+	}
+	for {
+		state := atomic.LoadUint32(&o.state)
+
+		if state == done {
+			return
+		}
+
+		futex.Wait(&o.state, running)
+	}
 }
 
 func (o *Once) Done() bool {
-	panic("не реализовано")
+	return atomic.LoadUint32(&o.state) == done
 }
