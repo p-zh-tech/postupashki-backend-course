9747425b35bfa80ce815513f983ac12ccd6521c4
p-zh-tech <kisly02@gmail.com>
2026-09-20 23:29:45 +0300

    Implement semaphore functionality in semaphore.go
    Implemented semaphore methods: Acquire, TryAcquire, Release, and Available.

--- concurrency/03_semaphore/semaphore.go
+++ concurrency/03_semaphore/semaphore.go
@@ -1,25 +1,63 @@
 package semaphore
 
+import (
+	"primitives/internal/futex"
+	"sync/atomic"
+)
+
 type Semaphore struct {
 	permits uint32
 }
 
 func New(n int) *Semaphore {
-	panic("не реализовано")
+	if n < 0 {
+		panic("negative semaphore size")
+	}
+	return &Semaphore{
+		permits: uint32(n),
+	}
 }
 
 func (s *Semaphore) Acquire() {
-	panic("не реализовано")
+	for {
+		current := atomic.LoadUint32(&s.permits)
+		if current > 0 {
+			if atomic.CompareAndSwapUint32(
+				&s.permits,
+				current,
+				current-1,
+			) {
+				return
+			}
+			continue
+		}
+		futex.Wait(&s.permits, 0)
+	}
 }
 
 func (s *Semaphore) TryAcquire() bool {
-	panic("не реализовано")
+	for {
+		current := atomic.LoadUint32(&s.permits)
+
+		if current == 0 {
+			return false
+		}
+
+		if atomic.CompareAndSwapUint32(
+			&s.permits,
+			current,
+			current-1,
+		) {
+			return true
+		}
+	}
 }
 
 func (s *Semaphore) Release() {
-	panic("не реализовано")
+	atomic.AddUint32(&s.permits, 1)
+	futex.Wake(&s.permits)
 }
 
 func (s *Semaphore) Available() int {
-	panic("не реализовано")
+	return int(atomic.LoadUint32(&s.permits))
 }
